fa46e9e293554922c4f2ba85fe4ccc560322d6b7
IIIBlackhartIII <blackhart1996@gmail.com>
2023-08-31 20:52:15 -0400

    Update PacketStatistics.py
    optimisations and visual changes to graph

--- PacketStatistics.py
+++ PacketStatistics.py
@@ -1,12 +1,11 @@
 import socket
-import statistics as stat
 import time
 import matplotlib.pyplot as plt 
 import matplotlib.animation as anim
-import matplotlib
-from threading import Thread, Event, Lock
+from threading import Thread
+import numpy as np
 
-## UDP Setup ##
+### UDP SETUP ###
 UDP_IP = "0.0.0.0" #listen on all available network interfaces
 UDP_RECEIVE_PORT = 6301 #choose port number
 
@@ -19,8 +18,9 @@ server.bind(receive_address)
 
 # Packet Buffer Init #
 delta_times = [] #list of delta times for stats analysis
+mean_time = 0 #used to display the mean
 max_length = 60 #max length of statistics list
-last_received = time.time() #stores time since last packet for delta timing 
+last_received = time.perf_counter() #stores time since last packet for delta timing 
 wait_packets = 12 #how many packets before we start outputting stats?
 ms_length = 5 #how many decimals to show in time values
 first_packet = True #bool to toggle what to do on first received
@@ -28,7 +28,8 @@ first_packet = True #bool to toggle what to do on first received
 stat_debug_text = "" #string for stat debugs
 
 def packet_timing_loop():
-    global delta_times 
+    global delta_times
+    global mean_time
     global max_length 
     global last_received
     global wait_packets
@@ -43,12 +44,12 @@ def packet_timing_loop():
         if (data != None):
             if (first_packet):
                 #if this is the first packet, only update our receive time, subsequent packets will be stored with deltas
-                last_received = time.time() #current time
+                last_received = time.perf_counter() #current time
                 first_packet = False #we have the first packet, don't run this block again
             else:
-                time_since_last = time.time() - last_received #calc delta time since last packet
+                time_since_last = time.perf_counter() - last_received #calc delta time since last packet
                 delta_times.append(time_since_last * 1000) #add to list of delta time for stats
-                last_received = time.time() #update the time since last to right now
+                last_received = time.perf_counter() #update the time since last to right now
 
                 #remove first index if list length too long
                 if (len(delta_times) > max_length):
@@ -57,46 +58,37 @@ def packet_timing_loop():
                 #start displaying stats if we have enough packets
                 if (len(delta_times) > wait_packets):
                     #statistics
-                    packet_mean = stat.mean(delta_times) 
-                    #packet_median = stat.median(delta_times) 
-                    #packet_variance = stat.variance(delta_times, packet_mean)
-                    packet_max = max(delta_times)
-                    packet_min = min(delta_times)
+                    packet_mean = np.mean(delta_times)                    
+                    packet_max = np.max(delta_times)
+                    packet_min = np.min(delta_times)
+                    mean_time = packet_mean
 
-                    #convert to strings for print
-                    packet_mean_str = convert_string_length(packet_mean,ms_length)
-                    #packet_median_str = convert_string_length(packet_median,ms_length)
-                    #packet_variance_str = convert_string_length(packet_variance,ms_length)
-                    packet_max_str = convert_string_length(packet_max,ms_length)
-                    packet_min_str = convert_string_length(packet_min,ms_length)
-
-                    #display statistics
-                    stat_debug_text = str("Mean = " + packet_mean_str + " / Min = " + packet_min_str + " ms / Max = " + packet_max_str + " ms")
-                print(stat_debug_text,  end="\r")
-
-def convert_string_length(input_float : float, length_limit : int):
-    string_convert = str(input_float)
-    return string_convert[:length_limit]
+                    #output log statistics
+                    stat_debug_text = str("Mean = {meanstr:.2f}ms / Min = {minstr:.2f}ms / Max = {maxstr:.2f}ms").format(meanstr = packet_mean, minstr = packet_min, maxstr = packet_max)
+                    print(stat_debug_text,  end="\r")
 
 print ("Listening For Packets!")
 
-## Plotting Setup ##
-matplotlib.rcParams['font.size'] = 8.0
-plt.style.use("fivethirtyeight")
+### PLOTTING SETUP ###
+plt.style.use('dark_background')
 fig = plt.figure(figsize=(16,9))
 ax = fig.add_subplot()
 
 def graph_anim(i):
-    ax.clear()
-    ax.eventplot(positions=[delta_times])
-    ax.set_xlabel("Delta Time (ms)" + " /// " + stat_debug_text)
-    ax.set_ylabel(" ")
-    ax.set_yticks([])
-    fig.canvas.draw()
+    ax.clear() #clear old chart
+    ax.eventplot(delta_times, colors=["b"]) #draw new chart data points
+    ax.set_xlabel(str("Delta Time (ms) /// {stats_text}").format(stats_text = stat_debug_text)) #set axis label with updating stats data
+    ax.set_ylabel(" ") #blank y-axis label
+    ax.set_yticks([]) #no y-axis ticks
+    plt.axvline(x=mean_time, color='white') #add a horizontal line at the mean
+    fig.canvas.draw() #draw the canvas
+
+
+### RUNTIME ###
 
 ## Multi Threading Setup! ##
 t1 = Thread(target = packet_timing_loop)
 t1.start()
 
-animgraph = anim.FuncAnimation(fig,graph_anim,interval=16,cache_frame_data=False)
+animgraph = anim.FuncAnimation(fig,graph_anim,interval=33,cache_frame_data=False)
 plt.show()
